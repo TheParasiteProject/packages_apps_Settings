9ecb66921be05c1f5945c650aaf862268826a27a
someone5678 <nemui3353@gmail.com>
2022-11-10 18:49:54 +0900

    Revert "Settings: Restore statusbar brightness preferences"
    This reverts commit ac2302ac642d35f7922304e3f97cbc79bb0dd65c.

--- src/com/android/settings/custom/statusbar/StatusBarSettings.java
+++ src/com/android/settings/custom/statusbar/StatusBarSettings.java
@@ -32,6 +32,7 @@ import androidx.preference.SwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
+import com.android.settings.custom.preference.SecureSettingListPreference;
 import com.android.settings.custom.preference.SystemSettingListPreference;
 import com.android.settings.SettingsPreferenceFragment;
 
@@ -44,7 +45,6 @@ public class StatusBarSettings extends SettingsPreferenceFragment
 
     private static final String CATEGORY_BATTERY = "status_bar_battery_key";
     private static final String CATEGORY_CLOCK = "status_bar_clock_key";
-    private static final String CATEGORY_BRIGHTNESS = "status_bar_brightness_category";
 
     private static final String ICON_BLACKLIST = "icon_blacklist";
 
@@ -53,7 +53,6 @@ public class StatusBarSettings extends SettingsPreferenceFragment
     private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
     private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
     private static final String STATUS_BAR_QUICK_QS_PULLDOWN = "qs_quick_pulldown";
-    private static final String STATUS_BAR_QUICK_QS_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
 
     private static final int STATUS_BAR_BATTERY_STYLE_TEXT = 2;
 
@@ -67,11 +66,8 @@ public class StatusBarSettings extends SettingsPreferenceFragment
     private SystemSettingListPreference mStatusBarBattery;
     private SystemSettingListPreference mStatusBarBatteryShowPercent;
 
-    private SwitchPreference mStatusBarQsShowAutoBrightness;
-
     private PreferenceCategory mStatusBarBatteryCategory;
     private PreferenceCategory mStatusBarClockCategory;
-    private PreferenceCategory mStatusBarBrightnessCategory;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -94,13 +90,6 @@ public class StatusBarSettings extends SettingsPreferenceFragment
         mQuickPulldown = findPreference(STATUS_BAR_QUICK_QS_PULLDOWN);
         mQuickPulldown.setOnPreferenceChangeListener(this);
         updateQuickPulldownSummary(mQuickPulldown.getIntValue(0));
-
-        mStatusBarBrightnessCategory = getPreferenceScreen().findPreference(CATEGORY_BRIGHTNESS);
-        mStatusBarQsShowAutoBrightness = mStatusBarBrightnessCategory.findPreference(STATUS_BAR_QUICK_QS_SHOW_AUTO_BRIGHTNESS);
-        if (!getResources().getBoolean(
-                com.android.internal.R.bool.config_automatic_brightness_available)){
-            mStatusBarBrightnessCategory.removePreference(mStatusBarQsShowAutoBrightness);
-        }
     }
 
     @Override
