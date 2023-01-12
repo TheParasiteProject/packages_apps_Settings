bb6ee754d9f75f1010825965ca872af5c7fd8e05
Ido Ben-Hur <idoybh2@gmail.com>
2023-01-12 20:10:28 +0200

    Settings: Declare an activity for WirelessDebuggingFragment
    we want the debugging is connected notification to open this page instead of just dev settings
    
    Change-Id: I30e4eb3c4519e421afd6bcf1d10f35b451baf1c1

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -91,6 +91,7 @@ import com.android.settings.datausage.DataUsageSummary;
 import com.android.settings.datetime.DateTimeSettings;
 import com.android.settings.deletionhelper.AutomaticStorageManagerSettings;
 import com.android.settings.development.DevelopmentSettingsDashboardFragment;
+import com.android.settings.development.WirelessDebuggingFragment;
 import com.android.settings.deviceinfo.PrivateVolumeForget;
 import com.android.settings.deviceinfo.PublicVolumeSettings;
 import com.android.settings.deviceinfo.StorageDashboardFragment;
@@ -379,7 +380,8 @@ public class SettingsGateway {
             ScreenTimeoutSettings.class.getName(),
             LiveDisplaySettings.class.getName(),
             WifiTetherClientManager.class.getName(),
-            SmartPixelsSettings.class.getName()
+            SmartPixelsSettings.class.getName(),
+            WirelessDebuggingFragment.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -427,5 +429,6 @@ public class SettingsGateway {
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
             Settings.SmartPixelsActivity.class.getName(),
+            Settings.WirelessDebuggingActivity.class.getName(),
     };
 }
