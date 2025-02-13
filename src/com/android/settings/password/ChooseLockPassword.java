31be904d9b3ef3def0e88fc6217a2889d8ea47c4
Jason Chang <jasonsfchang@google.com>
2025-02-13 04:34:50 +0000

    Fix Talkback should not announce disruptive announcement "Re-enter your PIN" page.
    Remove the calling announceForAccessibility() to prevent disruptive
    announcement.
    
    Flag: EXEMPT bugfix
    
    Bug: 383259346
    Bug: 384608181
    
    Test: local build and verify Talkback announcement in
    "Re-enter your PIN" page.
    
    Change-Id: Ide29dde477a095db12eb959dc3d89b6689bd6ab2

--- src/com/android/settings/password/ChooseLockPassword.java
+++ src/com/android/settings/password/ChooseLockPassword.java
@@ -770,7 +770,7 @@ public class ChooseLockPassword extends SettingsActivity {
             // If the stage changed, announce the header for accessibility. This
             // is a no-op when accessibility is disabled.
             if (previousStage != stage) {
-                mLayout.announceForAccessibility(mLayout.getHeaderText());
+                getActivity().setTitle(mLayout.getHeaderText());
             }
         }
 
