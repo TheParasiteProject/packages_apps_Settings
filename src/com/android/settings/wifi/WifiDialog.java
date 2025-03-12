351e6a94e0a805bae68a878cf21da5ef7e6a0e47
Weng Su <wengsu@google.com>
2025-03-12 20:46:49 +0800

    Fixed accessibility issues in Wi-Fi password view for SUW
    - Keep the Save button enabled at all times
    
    - Show "*required" or "The password is invalid" to remind the user
    
    Bug: 386897596
    Flag: EXEMPT bugfix
    Test: Manual testing
      atest WifiConfigControllerTest
    Change-Id: I442d2f958efd85f3c92309d0bed7cd3aa9ec9876

--- src/com/android/settings/wifi/WifiDialog.java
+++ src/com/android/settings/wifi/WifiDialog.java
@@ -28,7 +28,6 @@ import android.widget.TextView;
 import androidx.appcompat.app.AlertDialog;
 
 import com.android.settings.R;
-import com.android.settings.wifi.utils.TextInputGroup;
 import com.android.settings.wifi.utils.WifiDialogHelper;
 import com.android.settingslib.RestrictedLockUtils;
 import com.android.settingslib.RestrictedLockUtilsInternal;
@@ -119,9 +118,7 @@ public class WifiDialog extends AlertDialog implements WifiConfigUiBase,
             mController.hideForgetButton();
         }
 
-        mDialogHelper = new WifiDialogHelper(this,
-                new TextInputGroup(mView, R.id.ssid_layout, R.id.ssid,
-                        R.string.vpn_field_required));
+        mDialogHelper = new WifiDialogHelper(this, mController.getValidator());
     }
 
     @SuppressWarnings("MissingSuperCall") // TODO: Fix me
