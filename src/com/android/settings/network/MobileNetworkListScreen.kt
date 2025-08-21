6ae9e76249aeb361d338ea61f49872ecaa60cf1e
tom hsu <tomhsu@google.com>
2025-08-21 17:40:34 +0000

    [Catalyst] Add catalyst preference for Mobile data
    Flag: EXEMPT bug fix
    fix: b/419310756
    Test: atest
    Change-Id: Ide907bea65746d1da817c581a9666ead950e8e5e

--- src/com/android/settings/network/MobileNetworkListScreen.kt
+++ src/com/android/settings/network/MobileNetworkListScreen.kt
@@ -189,6 +189,7 @@ open class MobileNetworkListScreen(context: Context) :
             }
             +SimCallsPreference() order +130
             +SimSmsPreference() order +140
+            +SimMobileDataPreference() order +150
         }
 
     companion object {
